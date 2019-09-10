13283abf018f1a2e61fe4b5ff25e85cbc991abef
Ty Jenkins <cientifik85@gmail.com>
2019-09-10 11:59:01 -0400

    restructuring project for public demo purposes

---
+++ functions.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const ejs = require('ejs');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+//Checks to see if cut-off date has passed.
+const checkDate = () => {
+var today = new Date();
+var idate = new Date('2020-01-01'); //Cutoff date
+idate.setHours(today.getHours());
+idate.setMinutes(today.getMinutes());
+idate.setSeconds(today.getSeconds());
+idate.setMilliseconds(today.getMilliseconds());
+
+// Parsing the date objects.
+today = Date.parse(today);
+idate = Date.parse(idate);
+
+// Comparisons.
+if (idate == today) {
+    timeCheck = 'Date is today.';
+    return timeCheck;
+}
+else if (idate < today) {
+    const timeCheck =  'Date is in the past.';
+    return timeCheck;
+}
+else if (idate > today) {
+    const timeCheck =  'Date is in the future.';
+    return timeCheck;
+}
+};
+
+//Checks to see if input field is empty and notifies user accordingly
+const inputCheck = (field) => {
+	for (let i = 0; i < field.length; i++) {
+		field[i] = field[i].trim();
+		if (!field[i].length) {
+			const check = 'Something is empty!';
+			return check;
+		}
+	}
+}
+
+//Checks to see if email is properly formatted
+const validEmail = (email) => {
+	const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	email = email.trim();
+	return regex.test(email);
+}
+
+//Sends email to person who just RSVP'd
+const sendEmail = (recipient, session) => {
+
+	const transporter = nodemailer.createTransport(sgTransport({
+	auth: {
+        api_user: process.env.SGUSER,
+	    api_key: process.env.SGAPIKEY
+     }
+	}));
+
+	ejs.renderFile(__dirname + '/public/email.html', { session }, function (err, html) {
+		if (err) {
+			console.log(err);
+		} else {
+			const mailOptions = {
+		  	from: process.env.SGUSER, // sender address
+		  	to: recipient, // list of receivers
+		  	subject: "Jason and Margaret - 07.08.2020", // Subject line
+		  	html: html, // The email that is sent
+		  	attachments: [{
+		     filename: 'fancyheader.png',
+		     path: __dirname +'/public/images/fancyheader.png',
+		     cid: 'fancyheader' 
+			}]
+			};
+
+			transporter.sendMail(mailOptions, function (err, info) {
+		   		if(err)
+		     	console.log(err)
+		   		else
+		     	console.log(info);
+			});
+		}
+	}
+	)
+};
+
+//Functions exported for use
+module.exports = {
+	checkDate: checkDate,
+	inputCheck: inputCheck,
+	validEmail: validEmail,
+	sendEmail: sendEmail
+}
---
+++ server.js
@@ -0,0 +1,119 @@
+//Module imports
+const knex = require('knex');
+const express = require('express');
+const session = require('express-session');
+const redis = require('redis');
+const url = require('url');
+const RedisStore = require('connect-redis')(session);
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const dotenv = require('dotenv').config();
+const nodemailer = require('nodemailer');
+const { check, validationResult } = require('express-validator');
+const fs = require('fs');
+const sgTransport = require('nodemailer-sendgrid-transport');
+
+//Importing external functions
+const functions = require('./functions.js');
+
+//bodyParser
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
+//Call Express, store it in app and set port number
+const app = express();
+const port = 3306; 
+
+//Set view engine
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public');
+app.engine('html', ejs.renderFile);
+
+//Open port
+app.listen(process.env.PORT || port, () => {
+	console.log("app is working on port " + port);
+});
+
+//Set session
+const redisURL = url.parse(process.env.REDISTOGO_URL);
+const client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
+client.auth(redisURL.auth.split(":")[1]);
+app.use(session({
+  	resave: false,
+  	saveUninitialized: true,
+  	secret: 'asl84CxXZoIL93ame391',
+  	store: new RedisStore({client: client})
+}));
+
+//MySql database connection
+const db = knex({
+  client: 'mysql',
+  connection: {
+  	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_DB
+  }
+ });
+
+//Root route
+app.get('/', function (req, res) {
+	const date = functions.checkDate();
+	if (date === 'Date is in the past.') {
+		res.render('formclosed.html');
+	} else {
+		res.render('index', { session });
+	}
+});
+
+//Route to confirm information before being submitted
+app.post('/confirm', urlencodedParser, function (req, res) {
+	let session = req.session;
+	session.firstName = req.body.firstName;
+	session.lastName = req.body.lastName;
+	session.email = req.body.email;
+
+	const fieldArray = [session.firstName, session.lastName, session.email]
+
+	//Checking to see if any of the fields are empty (spaces don't count)
+	const inputCheck = functions.inputCheck(fieldArray);
+	if (inputCheck === 'Something is empty!') {
+		const message = "Hey, something isn't filled in! Please go back and fill in all the appropriate information. Thanks!";
+		res.render('oops', { message: message });
+		return;
+	}
+
+	//Checks to see if email is formatted properly
+	const validEmail = functions.validEmail(session.email);
+	if (validEmail === false) {
+		const message = "Your email address was not formatted properly! Please go back and check it!";
+		res.render('oops', { message: message });
+		return;
+	}
+
+	res.render('confirm', { session });
+})
+
+//Sends info to database then displays thank you page
+app.post('/rsvp', urlencodedParser, function(req, res) {
+	let session = req.session;
+	if (req.body.confirm === 'yes') {
+		const fullName = session.firstName + " " + session.lastName;
+		const email = session.email;
+
+		db('rsvps').insert(
+		{
+			name: fullName, 
+			email: email
+		}).then(()=>{});
+		// .finally(function() { db.destroy(); })
+
+		functions.sendEmail(session.email, session);
+		res.render('thankyou', { session });
+		session = null;
+
+	} else if (req.body.confirm === 'no') {
+		res.render('index', { session });
+	}
+});
+
+app.use(express.static(__dirname + '/public'));
